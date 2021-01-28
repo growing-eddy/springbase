ea301f100a0c9ef9bd02b20102b84dc0f20982eb
赵德刚 <363839586@qq.com>
2021-01-28 18:28:41 +0800

    feat: 提交aop的组件

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ tomcat1.8
 
 启动后访问地址：
 http://localhost:8080/base/home/index
+自动启动页面：
+http://localhost:8080/base/
 
 项目目标：
 使用基础的XML配置spring，可以了解其原理；
+
+
+遇到问题的解决方式：
+https://o-u-u.com/?p=196
+
+# xml aop没有生效
---
+++ src/main/java/example/common/aop/LogInterceptor.java
@@ -0,0 +1,34 @@
+package example.common.aop;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <PRE>
+ * {打印日志的aop}
+ * </PRE>
+ *
+ * @author zhaodg
+ * @date 2021/1/28
+ */
+@Component
+public class LogInterceptor {
+
+    public void myMethod(){};
+
+    /*@Before("execution(public void com.oumyye.dao.impl.UserDAOImpl.save(com.oumyye.model.User))")*/
+    public void before() {
+        System.out.println("method staet");
+    }
+
+    public void after() {
+        System.out.println("method after");
+    }
+
+    public void AfterReturning() {
+        System.out.println("method AfterReturning");
+    }
+    public void AfterThrowing() {
+        System.out.println("method AfterThrowing");
+    }
+
+}
